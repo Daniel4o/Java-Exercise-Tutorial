763a3d3daae5cc0738902b5c63bfb078504a0639
Daniel4o <danielddg.ss@gmail.com>
2022-10-13 17:16:01 +0300

    Started Collections section. Created a Collections overiew. Added an example for a Binary Search algorithm.

---
+++ Collections/CollectionsOveriew/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Olympian",8,12);
+       // theatre.getSeats();
+        if(theatre.reserveSeat("D12"))
+        System.out.println("Please pay");
+        else System.out.println("Sorry, seat is taken");
+    }
+}
