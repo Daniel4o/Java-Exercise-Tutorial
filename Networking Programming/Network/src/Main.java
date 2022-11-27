cc80b6900f8083a98d1a49f55750a011d7bfc60a
Daniel4o <danielddg.ss@gmail.com>
2022-11-27 17:01:35 +0200

    Finished Networking Programming section.

--- Networking Programming/Network/src/Main.java
+++ Networking Programming/Network/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -72,12 +74,65 @@ public class Main {
                 BufferedReader inputStream = new BufferedReader(
                         new InputStreamReader(urlConnection.getInputStream()));
 
-                String line ="";
+                Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+                for(Map.Entry<String,List<String>> entry : headerFields.entrySet()) {
+                    String key = entry.getKey();
+                    List<String> value = entry.getValue();
+                    System.out.println("------ key = " + key);
+                    for(String string : value) {
+                        System.out.println("value = " + value);
+                    }
+                }
+
+                String line = "";
                 while(line != null) {
                     line = inputStream.readLine();
                     System.out.println(line);
                 }
                 inputStream.close();
+
+            } catch(MalformedURLException e) {
+                System.out.println("Malformed URL: " + e.getMessage());
+            } catch(IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
+
+    class Main3 {
+
+        public static void main(String[] args) {
+            // Setting the response code for chrome, setting a timeout
+            // Then checking the response code if it's != 200
+            // THen there's an error reading the webpage otherwise
+            // inputReader will read the contents of the actual website
+            try {
+                URL url = new URL("https://api.flickr.com/services/feeds/photos_public.gne?tags=cats");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("User-Agent","Chrome");
+                connection.setReadTimeout(30000);
+
+                int responseCode = connection.getResponseCode();
+                System.out.println("Response code: " + responseCode);
+
+                if(responseCode != 200) {
+                    System.out.println("Error reading web page");
+                    System.out.println(connection.getResponseMessage());
+                    return;
+                }
+
+                BufferedReader inputReader = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+
+                String line ;
+
+                while((line = inputReader.readLine()) != null) {
+                    line = inputReader.readLine();
+                    System.out.println(line);
+                }
+
+                inputReader.close();
 
             } catch(MalformedURLException e) {
                 System.out.println("Malformed URL: " + e.getMessage());
